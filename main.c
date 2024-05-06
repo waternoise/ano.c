ac1e9a78186322ba4c5e513a7140f9216cee899d
r <nil>
2024-05-06 13:12:30 +0800

    poorly implemented arg parsing; getting specific item action left

--- main.c
+++ main.c
@@ -1,30 +1,101 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "db.h"
 
-int main() {
-	DBManager *manager = db_open("example.db");
-	if (!manager) {
-		fprintf(stderr, "Failed to open database\n");
-		return 1;
+// Function initializations...
+DBManager* start(); // Returns a manager to a DB file
+
+int main(int argc, char *argv[]) {
+	// Get a manager if SQLite3 can be used.
+	DBManager *manager = start();
+	// Put parsing things here for testing
+	int opt;
+	// Expected values to be given by the user
+	char *itemName = NULL;
+	char *newItemName = NULL;
+	char *newItemDescription = NULL;
+	double newItemPrice = 0.0;
+	// Flags
+	int listAllFlag = 0;
+	int resetFlag = 0;
+	int changeFlag = 0;
+	int insertFlag = 0;
+	int deleteFlag = 0;
+
+	// Process command-line arguments
+	while ((opt = getopt(argc, argv, "lm:ri:d:")) != -1) {
+		switch (opt) {
+			case 'l':
+				// wants to list all items
+				listAllFlag = 1;
+				break;
+			case 'm':
+				// wants to modify an item
+				itemName = optarg; // Original item name?
+				newItemName = argv[optind++];
+				newItemDescription = argv[optind++];
+				newItemPrice = strtod(argv[optind++], NULL);
+				changeFlag = 1;
+				break;
+			case 'i':
+				// wants to insert a new item
+				newItemName = optarg;
+				newItemDescription = argv[optind++];
+				newItemPrice = strtod(argv[optind++], NULL);
+				insertFlag = 1;
+				break;
+			case 'd':
+				// wants to delete an item
+				itemName = optarg;
+				deleteFlag = 1;
+				break;
+			case 'r':
+				// wants to reset the database table
+				resetFlag = 1;
+				break;
+			case '?':
+				// Handle unknown options or missing arguments
+				printf("Unknown option: %c\n", optopt);
+				db_close(manager);
+				break;
+			default:
+				// Handle any errors
+				db_close(manager);
+				break;
+		}
 	}
-	// Perform database operations...
-	// Delete this: db_initialize(manager);
-	// Not yet: db_reset(manager);
-	// Put here operations, remember to close it after.
-	//db_insert_item(manager, "ligo green", "sardinas na green", 25);
-	db_insert_item(manager, "green", "sardinas na green", 25);
-	db_insert_item(manager, "red", "sardinas na red", 28);
-	db_insert_item(manager, "joy", "blue syrup", 45.32);
-	db_list_all_items(manager);
-	db_get_item_by_name(manager, "red");
-	db_get_item_by_name(manager, "green");
-	db_get_item_by_name(manager, "lamp");
-	db_modify_item(manager, "red", NULL, NULL, 145.34);
-	db_modify_item(manager, "joy", "Joy Antibac", "dishwashing solution", 16);
-	db_modify_item(manager, "green", "Ligo Green", "sardinas na green", 30.24);
-	//db_modify_item(manager, "lol", NULL, NULL, -1);
-	db_list_all_items(manager);
+
+	// Call the appropriate functions based on the flags
+	if (listAllFlag) {
+		db_list_all_items(manager);
+	}
+	if (resetFlag) {
+		db_reset(manager);
+	}
+	if (changeFlag) {
+		db_modify_item(manager, itemName, newItemName, newItemDescription, newItemPrice);
+	}
+	if (insertFlag) {
+		db_insert_item(manager, newItemName, newItemDescription, newItemPrice);
+	}
+	if (deleteFlag) {
+		db_delete_item(manager, itemName);
+	}
+
+	// CLOSE THE DB CONNECTION AFTER EXITING THE PROGRAM
+	// ... where else should I put this? Is this safe?
 	db_close(manager);
 	return 0;
 }
 
+
+// Function returns a DB manager, be sure to close it no matter what happens.
+DBManager* start() {
+	DBManager *manager = db_open("items.db");
+	if (!manager) {
+		fprintf(stderr, "Failed to open database\n");
+		exit(EXIT_FAILURE);
+	}
+	return manager; // Indicate success.
+}
